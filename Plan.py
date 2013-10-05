587530d3c2850ab325f55e3e8ea2cd4cb1732c71
Wilmer Bandres <wilmer0593@hotmail.com>
2013-10-05 15:18:25 -0430

    Plan listo

--- Plan.py
+++ Plan.py
@@ -3,10 +3,12 @@ import sys
 
 class Plan(object):
     #Constructor de la clase
-    def __init__(self,descripcion,nombre,rif_empresa):
+    def __init__(self,descripcion,ilimitado,nombre,renta,rif_empresa):
         self.descripcion = descripcion
         self.nombre = nombre
         self.rif_empresa = rif_empresa
+        self.ilimitado = ilimitado
+        self.renta = renta
 
     def __eq__(self,otr):
         return self.nombre==otr.getNombre() and self.rif_empresa==otr.getRif_Empresa()
@@ -29,3 +31,15 @@ class Plan(object):
     
     def setRif_Empresa(self, rif_empresa):
 	self.rif_empresa = rif_empresa
+
+    def getIlimitado(self):
+        return self.ilimitado
+
+    def setIlimitado(self, ilimitado):
+        self.ilimitado = ilimitado
+
+    def getRenta(self):
+        return self.renta
+
+    def setRenta(self,renta):
+        self.renta = renta
